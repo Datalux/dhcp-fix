bed8a8765a6b572128050335ca28c1029ae066b6
Datalux <giuseppecriscionedev@gmail.com>
2018-07-15 19:54:19 +0200

    Graphics

--- main.py
+++ main.py
@@ -8,11 +8,17 @@ import os
 def runCommand():
 	os.system("dhclient -r")
 	os.system("dhclient")
-	msg = messagebox.showinfo("Done", "Fixed!")
+	msg = messagebox.showinfo("dhcp-fix", "Fixed!")
 
 def main():
 	root = Tk()
+	root.title("dhcp-fix")
 	root.geometry("256x256")
+	root.configure(background='#565')
+	
+	label = Label(root, text="Created by Datalux v0.1.1")
+	label.place(relx=0.5, rely=1, anchor='s')
+
 	if not os.geteuid() == 0:
 		messagebox.showinfo("Error","This script require root privileges")
 	else:
